d97133b33569b7d63a21b216ad1884aaa49237cc
James Harrington <JRileyHarrington@gmail.com>
2018-01-07 17:19:58 -0500

    real progress on client validation

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const server = require('http').createServer();
 const redis = require('redis');
 const db = redis.createClient();
+const db_events = redis.createClient();
+db_events.subscribe('__keyevent@0__:expired')
+db.config("SET","notify-keyspace-events", "Ex");
 const io = require('socket.io')(server);
-const expiration = 30;
+const expiration = 10;
 
 //===Connect to REDIS DB===\\
 db.on('connect', function(){
@@ -21,21 +24,24 @@ createCID = function(length, cb){
         cb(cid)
     });
 }
-newCID = function(sid){
+newCID = function(cb){
     createCID(24, function(cid){
         db.sadd('all_cids', cid);
-        io.sockets.connected[sid].emit('create_cid', {cid: cid, expiration: expiration);
-        connectUser({cid:cid, sid:sid});
+        cb(cid);
     });
 }
+
 connectUser = function(user){
-    if(user.hasOwnProperty('cid')){
+    if(user.hasOwnProperty('cid') && user.hasOwnProperty('sid')){
         for(var key in user){
             if(user.hasOwnProperty(key)){
                 db.hset('user:'+user.cid, key, user[key]);
             }
         }
         db.expire('user:'+user.cid, expiration);
+        io.sockets.connected[user.sid].emit('create_cid', {cid: user.cid, expiration: expiration});
+
+        //Do what ever connection means
         db.smembers('all_cids', function(err, cids){
             console.log('----------')
             for(var cid of cids){
@@ -48,21 +54,56 @@ connectUser = function(user){
     }
 }
 
-//===Incoming Client Socket Connections===\\
-io.on('connect', function(socket){
-    var cid = socket.handshake.query['cid'];
-    var sid = socket.id;
-    if(cid==='undefined'){
-        newCID(sid);
+db_events.on('message', function(event, entry){
+    var _event = event.split(':');
+    var _entry = entry.split(':');
+    switch(_event[1]){
+        case 'expired':
+            if(_entry[0]==='user'){
+                db.srem('all_cids', _entry[1]);
+            }
+        break;
+        default:
+        console.warn('Unhandled DB_EVENT: '+event+' '+entry);
+    }
+});
+
+validateClient = function(cid, cb){
+    if(typeof cid==='undefined' || cid==null){
+        newCID(function(new_cid){
+            cb(new_cid);
+        });
     } else {
         db.sismember('all_cids', cid, function(err, exists){
             if(err) throw err;
             if(!exists){
-                newCID(sid);
+                newCID(function(new_cid){
+                    cb(new_cid);
+                });
             } else {
-                connectUser({cid:cid, sid:sid});
+                cb(cid);
             }
         });
-    }   
+    }
+}
+
+//===Incoming Client Socket Connections===\\
+io.on('connect', function(socket){
+    validateClient(socket.handshake.query['cid'], function(cid){
+        connectUser({cid:cid, sid:socket.id});
+    });
+
+    
+    socket.on('host', function(data){
+        validateClient(data.cid, function(cid){
+            data.cid = cid;
+            connectUser(data);
+        })
+    });
+
+    
 });
+
+
+
 server.listen(1337, () => console.log('server listening on port 1337'));
